d951e25349e8ce08f3ff2fe3708170d1c083b38f
littlelienpeanut <littlelienpeanut@gmail.com>
2018-01-22 23:30:31 +0800

    Jan. 22 2018

--- supervise_demo_pred.py
+++ supervise_demo_pred.py
@@ -1,9 +1,12 @@
 import pandas as pd
+import itertools
 import csv
 from sklearn.neighbors import KNeighborsClassifier
 import random
 from sklearn.cross_validation import train_test_split
+from sklearn.model_selection import cross_val_predict
 from sklearn.model_selection import cross_val_score
+from sklearn.metrics import confusion_matrix
 from sklearn.metrics import roc_curve
 from sklearn.metrics import auc
 import matplotlib.pyplot as plt
@@ -113,6 +116,41 @@ def get_class_num(te_y):
 
     return tmp_list
 
+def plot_confusion_matrix(cm, classes,
+                          normalize=False,
+                          title='Confusion matrix',
+                          cmap=plt.cm.Blues):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
+
+    print(cm)
+
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, format(cm[i, j], fmt),
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+
+
 def main():
     #variable
     data_v4 = []
@@ -167,13 +205,14 @@ def main():
     #knn
     #age
     class_num = []
+    age_class_name = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"]
     knn_v4_age = KNeighborsClassifier(n_neighbors = 16)
-    tr_x, te_x, tr_y, te_y = train_test_split(data_v4, user_label_age, test_size = 0.2, random_state=42)
-    class_num = get_class_num(te_y)
-    knn_v4_age.fit(tr_x, tr_y)
-    print(knn_v4_age_fpr)
-    print(knn_v4_age_tpr)
-
+    knn_v4_age_pred = cross_val_predict(knn_v4_age, data_v4, user_label_age, cv=5)
+    print(knn_v4_age_pred)
+    cnf_matrix = confusion_matrix(user_label_age, knn_v4_age_pred)
+    plt.figure()
+    plot_confusion_matrix(cnf_matrix, classes=age_class_name, normalize=True, title='Confusion matrix, without normalization')
+    plt.show()
 
 
 if __name__ == '__main__':
