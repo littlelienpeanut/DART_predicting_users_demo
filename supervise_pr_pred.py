7e26ce18fff0f7f8ffb873da03f046c163745f1a
littlelienpeanut <littlelienpeanut@gmail.com>
2018-03-11 20:39:03 +0800

    Mar. 11 2018

--- supervise_pr_pred.py
+++ supervise_pr_pred.py
@@ -3,6 +3,8 @@ import itertools
 import csv
 import random
 from sklearn.cross_validation import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn import linear_model
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import cross_val_predict
 from sklearn.model_selection import cross_val_score
@@ -12,16 +14,21 @@ from sklearn.metrics import auc
 import matplotlib.pyplot as plt
 import numpy as np
 from sklearn import svm
+from sklearn.metrics import mean_squared_error
+from math import sqrt
 
 
 def load_data_v4(usernum):
+    #can not just append float to list, must have to change the list to array and set dtype = float64.
     daily_v4 = []
-
     for fname in usernum:
         tmp_list = []
+        tmp_list = np.array(tmp_list)
         data = pd.read_csv(fname + "_daily_v4.csv")
         for j in range(0, 24, 1):
-            tmp_list.append( "%.3f" % data["ratio"][j])
+            tmp_list = np.append(tmp_list, "%.3f" % float(data["ratio"][j]))
+
+        tmp_list.dtype = 'float64'
         daily_v4.append(tmp_list)
 
     return daily_v4
@@ -32,9 +39,12 @@ def load_data_v5(usernum):
 
     for fname in usernum:
         tmp_list = []
+        tmp_list = np.asarray(tmp_list)
         data = pd.read_csv(fname + "_daily_v5.csv")
         for cate in cate_list:
-            tmp_list.append( "%.3f" % data[cate][0])
+            tmp_list = np.append(tmp_list, "%.3f" % float(data[cate][0]))
+
+        tmp_list.dtype = 'float64'
         daily_v5.append(tmp_list)
 
     return daily_v5
@@ -72,28 +82,43 @@ def load_pr():
     Agr = []
     Con = []
     Ope = []
+
+    HH = np.array(HH)
+    Emo = np.array(Emo)
+    Ext = np.array(Ext)
+    Agr = np.array(Agr)
+    Con = np.array(Con)
+    Ope = np.array(Ope)
+
     for i in range(len(csv)):
-        HH.append(csv["HH"][i])
-        Emo.append(csv["Emo"][i])
-        Ext.append(csv["Ext"][i])
-        Agr.append(csv["Agr"][i])
-        Con.append(csv["Con"][i])
-        Ope.append(csv["Ope"][i])
+        HH = np.append(HH, csv["HH"][i])
+        Emo = np.append(Emo, csv["Emo"][i])
+        Ext = np.append(Ext, csv["Ext"][i])
+        Agr = np.append(Agr, csv["Agr"][i])
+        Con = np.append(Con, csv["Con"][i])
+        Ope = np.append(Ope, csv["Ope"][i])
 
     return HH, Emo, Ext, Agr, Con, Ope
 
 
 def main():
     #variable
+    print("Loading users' info")
     usernum = choose_user()
     data_v4 = load_data_v4(usernum)
     data_v5 = load_data_v5(usernum)
     #big6
+    print("Loading pr_info")
     HH, Emo, Ext, Agr, Con, Ope = load_pr()
 
     #main
-    clf_log = LogisticRegression(C = 2.0)
-    clf_log_pred = cross_val_predict(clf_log, data_v4, HH, cv=5)
+    clf_log = linear_model.LinearRegression()
+    clf_log_pred = cross_val_predict(clf_log, data_v5, HH, cv=3)
+    #print(data_v5)
+    #input()
+    #print(HH)
+    #input()
+    print(clf_log_pred)
 
 
 if __name__ == '__main__':
